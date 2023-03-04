4cb6e0a50b3e42f1edf2be4d56392778fabdd8ac
Oliver <64903691+olvr1812@users.noreply.github.com>
2023-03-04 17:12:30 +0300

    Initial Commit

---
+++ MyBodyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBodyApp
+//
+//  Created by Оливер Салихов on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
